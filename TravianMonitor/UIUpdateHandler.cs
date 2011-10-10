33c40442e6f1a8ed9450a33a31c912f8ea51e39d
Churchill Yik <476418125@qq.com>
2011-10-10 18:13:30 +0800

    develop ui update event

---
+++ TravianMonitor/UIUpdateHandler.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Administrator
+ * Date: 2011-10-7
+ * Time: 14:45
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TravianMonitor
+{
+	public enum UIUpdateTypes
+	{
+		TroopsMonitorLog,
+		TroopsSendingLog,
+		VillageList,
+		TroopSendingList,
+	};
+    
+    public class UIUpdateArgs : EventArgs
+    {
+    	public UIUpdateTypes uiType;
+    };
+    
+    public class LogArgs : UIUpdateArgs
+    {
+        public string strLog;
+    };
+    
+	partial class TravianAccessor
+	{
+		public event EventHandler<UIUpdateArgs> OnUIUpdate;
+        public void DebugLog(string log, UIUpdateTypes type)
+        {
+            LogArgs arg = new LogArgs();
+            arg.strLog = log;
+            arg.uiType = type;
+            OnUIUpdate(this, arg);
+        }
+        
+        public void UIUpdate(UIUpdateTypes type)
+        {
+        	UIUpdateArgs arg = new UIUpdateArgs();
+        	arg.uiType = type;
+        	OnUIUpdate(this, arg);
+        }
+	}
+}
